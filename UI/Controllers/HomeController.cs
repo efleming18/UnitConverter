4b4af204690f99572521ac4b3ca9a679dc626ba9
Rich <richhildebrand@gmail.com>
2013-01-06 18:53:50 -0500

    Added unit test for HomeController

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -4,27 +4,24 @@ using System.Web.Mvc;
 
 namespace UI.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+   public class HomeController : Controller {
 
-            return View();
-        }
+      public ActionResult Index() {
+         ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
+         return View("Index");
+      }
 
-            return View();
-        }
+      public ActionResult About() {
+         ViewBag.Message = "Your app description page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+         return View();
+      }
 
-            return View();
-        }
-    }
+      public ActionResult Contact() {
+         ViewBag.Message = "Your contact page.";
+
+         return View();
+      }
+   }
 }
---
+++ UnitTests/UI/ForHomeController/IndexShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using NUnit.Framework;
+using UI.Controllers;
+
+namespace UnitTests.UI.ForHomeController
+{
+   [TestFixture]
+   class IndexShould
+   {
+      [Test]
+      public void ReturnTheIndexView()
+      {
+         var homeController = new HomeController();
+         ViewResult viewResult = (ViewResult)homeController.Index();
+         Assert.AreEqual("Index", viewResult.ViewName);
+      }
+   }
+}
